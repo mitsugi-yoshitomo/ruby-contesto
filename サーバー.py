cd99193e6f0458cd1f0e5c59978910b2380306d1
mitsugi-yoshitomo <three.next.2009@icloud.com>
2025-07-31 08:34:56 +0000

    サーバー追加

---
+++ サーバー.py
@@ -0,0 +1,15 @@
+# モジュール
+import random
+import flask
+
+app = flask.Flask(__name__)
+
+# 記事一覧のページ
+@app.route('/', methods=['GET'])
+def view_articles_route():
+  # メッセージを入れる
+  random_int = random.randint(1,100)
+  # 記事一覧のページを表示する
+  return str(random_int)
+
+app.run(host='0.0.0.0',port=8080)
